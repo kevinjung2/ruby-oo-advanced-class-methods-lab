51d1b97e07c10cc580988777f11bd85636854760
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-19 21:22:32 -0500

    commit tree a90b719222baf5cf2c59cb25fbc879dd2d5118c8

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,66 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    @@all << song
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name).is_a?(Song)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
   def self.all
     @@all
+  end
+
+  def self.alphabetical
+    @@all.sort { |a, b| a.name <=> b.name }
+  end
+
+  def self.new_from_filename(file)
+    song = self.new
+    split = file.split(/[-.]/)
+    split = split.collect { |str| str.strip }
+    song.artist_name = split[0]
+    song.name = split[1]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.new
+    split = file.split(/[-.]/)
+    split = split.collect { |str| str.strip }
+    song.artist_name = split[0]
+    song.name = split[1]
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
   def save
